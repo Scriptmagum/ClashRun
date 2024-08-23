c18511cf7b0b41a4bb1c780c2378dd1662e14402
ridiwane MAMA TOURE <ridiwanemamatoure@gmail.com>
2024-08-23 04:53:53 +0100

    d

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -1,11 +1,9 @@
 
 clear
-echo -e "\033[1;36m updating...\033[0m"
+echo -e "\033[1;36m updating...\033[1;37m"
 sleep 3
-cd ..
-rm -rf ClashRun
-git clone 'https://github.com/Scriptmagum/ClashRun.git'
+rm -f *
+echo -e "\033[1;36m cloning...\033[1;37m"
+sleep 2
+git clone 'https://github.com/Scriptmagum/ClashRun.git' .
 sleep 1.5
-clean
-cd ClashRun
-python3 Clashrun.py
